98282192aee13360e7cd47c6f1c7774c8b3b52d1
Mario Codes <mariocodes1993@gmail.com>
2025-11-27 09:46:01 +0100

    change regex for json

--- OpenAIApi/OpenAIApi.py
+++ OpenAIApi/OpenAIApi.py
@@ -1,34 +1,18 @@
-import re;
+import json;
 
 # suppose this is an Open's AI response
 openai_result = '''
-* Introduction
-  a. Explanation of data engineering
-  b. Importance of data engineering in today's data driven world
-* Efficient Data Management
-  a. Definition of data management
-  b. How data engineering helps in efficient data management
-* Conclusion
-  a. Importance of data engineering in the modern business world
-  b. Future of data engineering and its impact on the data ecosystem
+{
+    "Introduction": [
+        "a. Explanation of data engineering",
+        "b. Importance of data engineering in today's data driven world"
+    ],
+    "Conclusion": [
+        "a. Importance of data engineering in the modern business world",
+        "b. Future of data engineering and its impact on the data ecosystem"
+    ]
+}
 '''
 
-# regex patterns
-section_regex = re.compile(r"\* (.+)")
-subsection_regex = re.compile(r"\s*([a-z]\..+)")
-
-result_dict = {}
-current_section = None
-
-# print results
-for line in openai_result.split("\n"):
-    section_match = section_regex.match(line)
-    subsection_match = subsection_regex.match(line)
-
-    if section_match:
-        current_section = section_match.group(1)
-        result_dict[current_section] = []
-    elif subsection_match and current_section is not None:
-        result_dict[current_section].append(subsection_match.group(1))
-
-print(result_dict)
+parsed_json_payload = json.loads(openai_result)
+print(parsed_json_payload)

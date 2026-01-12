9780de1de4d92bdda5bbdb3e4f0f98c9e9e9af82
Mario Codes <mariocodes1993@gmail.com>
2026-01-12 13:03:18 +0100

    add LangChain class

---
+++ PromptEngineering/LangChain.py
@@ -0,0 +1,14 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import AIMessage, SystemMessage, HumanMessage
+
+def main():
+    chat = ChatOpenAI(temperature=0.9, model="gpt-4o-mini")
+    messages = [
+        SystemMessage(content="Act as a senior software engineer at a startup company"),
+        HumanMessage(content="Please can you provide a random funny dark joke about engineers?")
+    ]
+    response = chat.invoke(messages)
+    print(response.content)
+
+if __name__ == "__main__":
+    main()
